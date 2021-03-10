2b8e57a01ee1f666c1106c7991f40e0a85c74530
Jesse Wenger <wengerj@gmail.com>
2021-03-10 18:53:15 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,6 +1,6 @@
 class ChattyStudent < Student
   def hello 
     super
-    puts "Howare you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+    puts "How are you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
 end
